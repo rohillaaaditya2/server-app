d2c73ad5a5ce9f887674e2a8d4191a9b544f3eac
Aaditya <yourgmail@gmail.com>
2026-02-10 23:54:45 +0530

    final server update

--- server.js
+++ server.js
@@ -1,35 +1,70 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
+
 const config = require("./DB.js");
 
+// ROUTES
+const stateRoute = require("./admin/state.route.js");
+const cityRoute = require("./admin/city.route.js");
+const productcatgRoute = require("./admin/productcatg.route.js");
+const billRoute = require("./admin/bills/bill.route.js");
+const paymentdetailsRoute = require("./admin/bills/paymentdetails.route.js");
+
 const productRoute = require("./product/product.route.js");
-const venderRoute = require("./vender/vender.route.js");
-const customerRoute = require("./customer/customer.route.js");
 const inventoryRoute = require("./product/Inventory.route.js");
+
+const venderRoute = require("./vender/vender.route.js");
 const saleRoute = require("./vender/sales.route.js");
+
+const customerRoute = require("./customer/customer.route.js");
+
+const PaymentRoute = require("./payment.js");
+const emailRoute = require("./emailmgt.js");
+const emailactivationRoute = require("./emailactivation.js");
 
 const PORT = process.env.PORT || 9876;
 
+// MIDDLEWARE
 app.use(cors());
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-/* ROUTES */
+// STATIC FOLDER
+app.use(
+  "/productimages",
+  express.static(path.join(__dirname, "product/productimages"))
+);
+
+// ROUTES
+app.use("/state", stateRoute);
+app.use("/city", cityRoute);
+app.use("/productcatg", productcatgRoute);
 app.use("/product", productRoute);
-app.use("/vender", venderRoute);
-app.use("/customer", customerRoute);
 app.use("/inventory", inventoryRoute);
+
+app.use("/vender", venderRoute);
 app.use("/sales", saleRoute);
 
-/* DATABASE */
+app.use("/customer", customerRoute);
+
+app.use("/bill", billRoute);
+app.use("/paymentdetails", paymentdetailsRoute);
+
+app.use("/payment", PaymentRoute);
+app.use("/email", emailRoute);
+app.use("/emailactivation", emailactivationRoute);
+
+// DATABASE
 mongoose
   .connect(config.URL, { useNewUrlParser: true })
-  .then(() => console.log("DATABASE CONNECTED"))
+  .then(() => console.log("DATABASE IS CONNECTED"))
   .catch((err) => console.log("DB ERROR:", err));
 
+// SERVER
 app.listen(PORT, () => {
   console.log("SERVER RUNNING ON PORT " + PORT);
 });
